de7d208a5a43f20350ca9951651f8ad8f819664d
Michael McClimon <82418591+michaelmcclimon@users.noreply.github.com>
2021-08-02 20:12:33 -0400

    Uploading Channels main.go file

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"htpp://stackoverflow.com",
+		"htpp://golang.org",
+		"htpp://amazon.com",
+	}
+
+	c := make(chan string) 
+
+	for _, link := range links {
+		go checkLink(link, c)
+
+		for i := 0; i < len(links); i++ {
+			fmt.Println(<-c)
+		}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "Might be down!")
+		c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, "Is up!")
+	c <- "Yep its up"
+}
